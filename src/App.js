419838563576c8c3a15d3b8f84e54a0b003b9dcb
willnickerson <wnickers@willamette.edu>
2017-08-14 14:59:09 -0700

    styling and links to repos/npm/personal site added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-// import RainComponent from 'react-rain-animation';
-import RainComponent from './components/Rain';
+import RainComponent from 'react-rain-animation';
 import Knob from 'react-canvas-knob';
 import logo from './assets/coffee-cup-white.svg';
 import './App.css';
 import 'react-rain-animation/lib/style.css';
+
+const initValue = 25;
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      numDrops: 500,
-      knobValue: 500
+      numDrops: initValue,
+      knobValue: initValue
     };
   }
 
@@ -24,24 +25,45 @@ class App extends Component {
     console.log(this.state.numDrops);
     this.setState({numDrops: this.state.knobValue});
   }
-
+  
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <a href="http://willnickerson.com/">
+            <img src={logo} className="App-logo" alt="logo" />
+          </a>
           <h2>react-rain-animation demo</h2>
+          <ul>
+            <li>
+              <a href="https://github.com/willnickerson/react-rain-animation">Github repo</a>
+            </li>
+            <li>
+              <a href="https://www.npmjs.com/package/react-rain-animation">npm</a>
+            </li>
+            <li>
+              <a href="http://willnickerson.com">My website</a>
+            </li>
+          </ul>
         </div>
-        <RainComponent numDrops={this.state.numDrops}/>
+        <div className="App-body">
+          <p>
+            Play with the knob to change the number of drops
+          </p>
+          <RainComponent numDrops={this.state.numDrops}/>
+          <Knob className="Knob"
+            onChange={this.handleChange}
+            onChangeEnd={this.onChangeEnd}
+            value={this.state.knobValue}
+            thickness={.07}
+            width={125}
+            height={125}
+            min={0}
+            max={1000}
+            step={1}
+          /> 
 
-        <Knob className="Knob"
-          onChange={this.handleChange}
-          onChangeEnd={this.onChangeEnd}
-          value={this.state.knobValue}
-          min={0}
-          max={1000}
-          step={1}
-        /> 
+        </div>
       </div>
     );
   }
